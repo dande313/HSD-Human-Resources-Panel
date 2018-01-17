67a23f7ffa911fe3d524d9130000baded1edbcaf
HSD\aanderson <aanderson@hollandspecial.com>
2018-01-17 10:36:25 -0500

    added option to filter by any field

--- src/app/components/applicant-list/applicant-list.component.ts
+++ src/app/components/applicant-list/applicant-list.component.ts
@@ -14,6 +14,7 @@ export class ApplicantListComponent implements OnInit {
   dataSource: any;
   rows = [];
   temp = [];
+  filterType: any;
   columns = [
     { name: 'Name', prop: 'name' },
     { name: 'Department', prop: 'department' },
@@ -31,19 +32,33 @@ export class ApplicantListComponent implements OnInit {
       console.log(applicants);
       this.temp = [...applicants];
       this.rows = applicants;
+      this.filterType = 'name';
     });
   }
 
   updateFilter(event) {
-    console.log(event)
     const val = event.target.value.toLowerCase();
-
+    const filterType = this.filterType;
     // filter our data
     const temp = this.temp.filter(function(d) {
-      return (d.lastName + d.firstName).toLowerCase().indexOf(val) !== -1 || !val;
+      switch (filterType) {
+        case 'name':
+          return (d.lastName + d.firstName).toLowerCase().indexOf(val) !== -1 || !val;
+        case 'email':
+          return (d.email).toLowerCase().indexOf(val) !== -1 || !val;
+        case 'phone':
+          return (d.phone).indexOf(val) !== -1 || !val;
+        case 'location':
+          return (d.city + d.state).toLowerCase().indexOf(val) !== -1 || !val;
+        default :
+          return (d.lastName + d.firstName).toLowerCase().indexOf(val) !== -1 || !val;
+      }
     });
-
     // update the rows
     this.rows = temp;
+  }
+
+  updateFilterType(event) {
+    this.filterType = event.target.value;
   }
 }
