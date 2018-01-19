fe944e42791729ced0b47663a616405f5b7564ea
HSD\aanderson <aanderson@hollandspecial.com>
2018-01-19 08:18:26 -0500

    added filter to positions

--- src/app/components/positions-list/positions-list.component.ts
+++ src/app/components/positions-list/positions-list.component.ts
@@ -13,6 +13,8 @@ import {ViewEncapsulation} from '@angular/core';
 export class PositionsListComponent implements OnInit {
   dataSource: any;
   rows = [];
+  temp = [];
+  filterType: any;
   columns = [
     { name: 'Title', prop: 'title' },
     { name: 'Department', prop: 'department' },
@@ -25,6 +27,32 @@ export class PositionsListComponent implements OnInit {
     this.positionService.getPositions().subscribe(positions => {
       console.log(positions);
       this.rows = positions;
+      this.temp = [...positions];
+      this.filterType = 'name';
     });
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+    const filterType = this.filterType;
+    // filter our data
+    const temp = this.temp.filter(function(position) {
+      switch (filterType) {
+        case 'title':
+          return (position.title).toLowerCase().indexOf(val) !== -1 || !val;
+        case 'department':
+          return (position.department).toLowerCase().indexOf(val) !== -1 || !val;
+        case 'location':
+          return (position.city + position.state).toLowerCase().indexOf(val) !== -1 || !val;
+        default :
+        return (position.title).toLowerCase().indexOf(val) !== -1 || !val;
+      }
+    });
+    // update the rows
+    this.rows = temp;
+  }
+
+  updateFilterType(event) {
+    this.filterType = event.target.value;
   }
 }
