98995e426b7b1e2c588639c27835ac9f932d497a
Allen Anderson <dande313@byu.net>
2018-06-12 21:49:50 -0400

    Added environment keys back in

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyDII7eo3DCTZ7keFipcFfeiPcJrTDBxc24',
+      authDomain: 'hsd-hr-panel.firebaseapp.com',
+      databaseURL: 'https://hsd-hr-panel-59c17.firebaseio.com',
+      projectId: 'hsd-hr-panel-59c17',
+      storageBucket: '',
+      messagingSenderId: '581426375616'
+    },
+    openWeather: {
+      appId: 'c811e280edc98620a42c516086b451ba',
+      baseUrl: 'https://api.openweathermap.org/data/2.5/',
+    }
+  };
