103c117494cdbb4a1fff9d7f762739e9d452907b
Allen Anderson <dande313@byu.net>
2018-06-12 22:19:49 -0400

    fixed applicant model

--- src/app/services/applicant.service.ts
+++ src/app/services/applicant.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-import { Applicant } from '../models/Applicant';
+import { Applicant } from '../models/applicant';
 
 
 
